3d29ef21ad1e529fc15760085b4436a788cc7a2f
Shin Megami Tensei <megami@tenseimegami.com>
2023-06-11 15:33:55 +0200

    Update NotifHolder.lua

--- NotifHolder.lua
+++ NotifHolder.lua
@@ -1,23 +1,21 @@
-if not shared.WhereToPut then
 shared.WhereToPut = nil;
 if gethui and type(gethui) == "function" then
   shared.WhereToPut = gethui()
   else
-  shared.WhereToPut = game.CoreGui
+  shared.WhereToPut = game:GetService("CoreGui")
 end
-
+  
 if not game:GetService("CoreGui"):FindFirstChild("STX_Nofitication") then
     shared.STX_Nofitication = Instance.new("ScreenGui")
     shared.STX_NofiticationUIListLayout = Instance.new("UIListLayout")
     shared.STX_Nofitication.Name = "STX_Nofitication"
-    shared.STX_Nofitication.Parent = WhereToPut
+    shared.STX_Nofitication.Parent = shared.WhereToPut
     shared.STX_Nofitication.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
     shared.STX_Nofitication.ResetOnSpawn = false
     
     shared.STX_NofiticationUIListLayout.Name = "STX_NofiticationUIListLayout"
-    shared.STX_NofiticationUIListLayout.Parent = STX_Nofitication
+    shared.STX_NofiticationUIListLayout.Parent = shared.STX_Nofitication
     shared.STX_NofiticationUIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
     shared.STX_NofiticationUIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
     shared.STX_NofiticationUIListLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
-end
 end
